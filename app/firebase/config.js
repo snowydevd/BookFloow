31d60898e0e19f34f2b410ae1ea59b3fe76e1c58
Snowie Dev <lautarochini4@gmail.com>
2024-08-28 20:37:41 -0300

    Firebase Added

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAjeZTgEFRND_sVbILtheUd4vbOro9BPxc",
-  authDomain: "bookfloow.firebaseapp.com",
-  projectId: "bookfloow",
-  storageBucket: "bookfloow.appspot.com",
-  messagingSenderId: "474290144524",
-  appId: "1:474290144524:web:b5cc3a54ecdfb3bfac7529",
-  measurementId: "G-YET6J5CZ23",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
