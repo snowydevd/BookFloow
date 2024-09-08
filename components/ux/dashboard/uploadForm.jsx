d418b07f6ee84f53e0c433c7ae77120e28237621
Snowie Dev <lautarochini4@gmail.com>
2024-09-08 20:56:53 -0300

    RiverForm updated

--- components/ux/dashboard/uploadForm.jsx
+++ components/ux/dashboard/uploadForm.jsx
@@ -5,111 +5,126 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Upload, BookOpen } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
 
-// FIRESTORE;
 // import { db } from "@/app/firebase/config";
 // import { collection, addDoc } from "firebase/firestore";
-// import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+// import { set } from "date-fns";
 
 export default function UploadForm() {
-  const [bookData, setBookData] = useState({
-    titulo: "",
-    autor: "",
-    paginas: "",
-    precio: "",
-    portada: null,
-  });
+  // const [bookData, setBookData] = useState({
+  //   titulo: "",
+  //   autor: "",
+  //   paginas: "",
+  //   precio: "",
+  //   portada: null,
+  // });
+  const [value, setValue] = useState();
   const [uploading, setUploading] = useState(false);
-
-  const handleChange = (e) => {
-    const { name, value, files } = e.target;
-    if (name === "portada") {
-      setBookData((prevData) => ({ ...prevData, portada: files[0] }));
-    } else {
-      setBookData((prevData) => ({ ...prevData, [name]: value }));
-    }
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setUploading(true);
-
-    const formData = new FormData();
-    formData.append("titulo", bookData.titulo);
-    formData.append("autor", bookData.autor);
-    formData.append("paginas", bookData.paginas);
-    formData.append("precio", bookData.precio);
-    formData.append("portada", bookData.portada);
-
-    console.log(formData);
 
     try {
-      const response = await fetch("/api/uploadbook", {
+      const res = await fetch("/api/uploadbook", {
         method: "POST",
-        body: formData,
+        body: JSON.stringify({
+          name: value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
 
-      if (!response.ok) {
-        throw new Error("Error al subir el libro");
-      }
-
-      const data = await response.json();
-      console.log("Libro subido con éxito:", data);
+      const data = await res.json();
+      console.log(data);
     } catch (error) {
       console.error("Error:", error);
-    } finally {
-      setUploading(false);
     }
   };
-  return (
-    <div className="min-h-screen  flex items-center justify-center p-4">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="titulo"
-          placeholder="Título del libro"
-          value={bookData.titulo}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          name="autor"
-          placeholder="Autor"
-          value={bookData.autor}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="number"
-          name="paginas"
-          placeholder="Número de páginas"
-          value={bookData.paginas}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="number"
-          name="precio"
-          placeholder="Precio"
-          value={bookData.precio}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="file"
-          name="portada"
-          accept="image/*"
-          onChange={handleChange}
-          required
-        />
 
-        <button type="submit" disabled={uploading}>
-          {uploading ? "Subiendo..." : "Subir Libro"}
-        </button>
-      </form>
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sube tu libro</CardTitle>
+          <CardDescription>
+            Agrega un libro a tu biblioteca para que diferentes usuarios puedan
+            comprarlo, rentarlo o intercambiarlo
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          {/* <form className="space-y-5">
+            <div>
+              <Label>Titulo</Label>
+              <Input
+                type="text"
+                name="titulo"
+                placeholder="Título del libro"
+                value={bookData.titulo}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <Label>Autor</Label>
+              <Input
+                type="text"
+                name="autor"
+                placeholder="Autor del libro"
+                value={bookData.autor}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <Label>Numero de paginas</Label>
+              <Input
+                type="number"
+                name="paginas"
+                placeholder="Numero de paginas"
+                value={bookData.paginas}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <Label>Precio</Label>
+              <Input
+                type="number"
+                name="precio"
+                placeholder="Precio"
+                value={bookData.precio}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <Label>Portada</Label>
+              <Input
+                type="file"
+                name="portada"
+                accept="image/*"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <Button type="submit" onClick={(e) => handleSubmit(e)}>
+                Subir
+              </Button>
+            </div>
+          </form> */}
+          <Input type="text" onChange={(e) => setValue(e.target.value)} />
+          <Button onClick={(e) => handleSubmit(e)}>Subir</Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
